e2092acba4560b17cf4c2cf8021455d2812107ab
Roshan Kumar Rawat <165247276+Roshankumarrawat@users.noreply.github.com>
2024-08-20 04:40:33 +0530

    add lorem20

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-img', { origin: 'lright' });
 
 const typed = new Typed ('.multiple-text', {
-    strings: ['Frontend Developer!', 'Programmer', 'Youtuber'],
+    strings: ['Frontend Developer', 'Data Scientists', 'Programmer Analysts'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
